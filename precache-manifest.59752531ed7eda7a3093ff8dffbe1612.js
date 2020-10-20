f561be73ae33254b8ade7a323dc091cc80ff9a72
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-10-20 23:18:09 +0400

    Updates

---
+++ precache-manifest.59752531ed7eda7a3093ff8dffbe1612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a82e8a3cb7791a5304ff7b3e9131de",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "50bd5edc93030cce343e",
+    "url": "/shopping/static/css/main.98a1224a.chunk.css"
+  },
+  {
+    "revision": "d91af60789ce83b29115",
+    "url": "/shopping/static/js/2.9e43e3cf.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/shopping/static/js/2.9e43e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bd5edc93030cce343e",
+    "url": "/shopping/static/js/main.19b48670.chunk.js"
+  },
+  {
+    "revision": "6ac79c6b644c292d9835",
+    "url": "/shopping/static/js/runtime-main.96282085.js"
+  }
+]);
